f608881c6edf6b4091775d3ce22932a2f9768406
Justin Bentley <justinmbentley@outlook.com>
2025-03-31 02:10:52 +1100

    Add project files.

---
+++ Sleep/Sleep.c
@@ -0,0 +1,14 @@
+// Sleep ~ Justin Bentley ~ 2025
+
+// Trigger Monitor Down Power State
+
+#include <Windows.h>
+
+#pragma comment(linker, "/SUBSYSTEM:WINDOWS")
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    Sleep(1000);
+    SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+    return 0;
+}
